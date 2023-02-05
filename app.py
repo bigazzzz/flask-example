541f351cfa32b3fb8eccc42db8827a73f357f3b7
bigazzzz <bigazzzz@gmail.com>
2023-02-05 22:41:19 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from kubernetes import config, client
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import json
+import os
+    
+class Stand(Resource):
+    def get(self):
+        if os.getenv('STAND') is None:
+            return "Not defined", 404
+        else:
+            return os.getenv('STAND'), 200
+
+class Pods(Resource):
+    def get(self, podname):
+        return "ok", 200
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(json.dumps(data), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+api.add_resource(Stand, "/stand")
+if __name__ == '__main__':
+    app.run(debug=True)
